dbf83d2647e1601ee9b85fd051471367210d9a31
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-03-29 23:39:46 +0530

    Refactor project page layout to include SearchHeader, FilterSidebar, and ProjectExplorer components with loading skeletons

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
-
+import { Suspense } from "react"
+import ProjectExplorer from "../../components/projects/project-explorer"
+import FilterSidebar from "../../components/projects/filter-sidebar"
+import SearchHeader from "../../components/projects/search-header"
+import { Skeleton } from "../../components/ui/skeleton"
 interface Props {
     
 }
 
 const page = (props: Props) => {
     return (
-        <div>
-            
-            <h1>all Projects Page</h1>
+       < div className="container mx-auto py-8 px-4">
+      <SearchHeader />
+
+      <div className="flex flex-col md:flex-row gap-6 mt-8">
+        <div className="w-full md:w-64 lg:w-72 flex-shrink-0">
+          <FilterSidebar />
         </div>
+
+        <div className="flex-1">
+          <Suspense
+            fallback={
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {Array(6)
+                  .fill(0)
+                  .map((_, i) => (
+                    <Skeleton key={i} className="h-[350px] rounded-xl" />
+                  ))}
+              </div>
+            }
+          >
+            <ProjectExplorer />
+          </Suspense>
+        </div>
+      </div>
+    </div>
     )
 }
 
