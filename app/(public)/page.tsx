a20754f5969bc7c734306592a43628cc58b17b23
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-04-08 22:12:25 +0530

    removed hero

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,14 +4,12 @@ import AllProjects from "@/components/home/all-projects";
 import { Brands } from "@/components/home/brands";
 import ExploreByDomain from "@/components/home/explore-by-domain";
 import FeaturedProjects from "@/components/home/featured-projects";
-import Hero from "@/components/home/hero";
 import ImpactStats from "@/components/home/impact-stats";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-12 pb-12">
-      <Hero />
       <About />
       <FeaturedProjects />
       <ImpactStats />
