1e0f651a02c8320facf0cf073d2e9fa6e29edc46
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-04-08 10:04:53 +0530

    Delete samplesession.ts

--- app/(auth)/samplesession.ts
+++
@@ -1,9 +0,0 @@
-//This was done until the auth context is done and userid is saved in local storage
-export const session = {
-    user: {
-        id: "9cf8fd66-1f52-4345-8ada-43b7ca39702e",
-        name: "Sample user",
-        email: "sampleuser@example.com",
-        role: "ADMIN"
-    }
-};
