db38eb7bad6b6cade6ec6b23596c8abd3c301da9
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-04-10 13:08:35 +0530

    updated images

--- components/home/carousel.tsx
+++ components/home/carousel.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 const images = [
   'https://images.unsplash.com/photo-1614728263952-84ea256f9679?q=80&w=2069&auto=format&fit=crop',
+  '/Projects/2.png',
   '/Lexi/1.jpg',
-  'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?q=80&w=2071&auto=format&fit=crop',
-  'https://images.unsplash.com/photo-1447433589675-4aaa569f3e05?q=80&w=2080&auto=format&fit=crop',
+  '/Projects/1.PNG',
   'https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=2013&auto=format&fit=crop',
 ];
 
