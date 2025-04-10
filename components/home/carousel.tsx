a23a78b0e24fa2e416daffe513f3028cecad91bc
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-04-10 15:32:53 +0530

    fix image moves

--- components/home/carousel.tsx
+++ components/home/carousel.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 const images = [
   'https://images.unsplash.com/photo-1614728263952-84ea256f9679?q=80&w=2069&auto=format&fit=crop',
-  '/Projects/2.png',
-  '/Lexi/1.jpg',
-  '/Projects/1.PNG',
+  '/home/hero/raspberry.png',
+  '/home/hero/lexi.jpg',
+  '/home/hero/novalith.png',
   'https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=2013&auto=format&fit=crop',
 ];
 
@@ -24,10 +24,10 @@ export default function Carousel() {
       >
         {duplicatedImages.map((image, index) => (
             <div key={index} className="flex-none relative rounded-lg overflow-hidden w-[400px] aspect-video">
-            <img
+            <Image
               src={image}
               alt={`Carousel image ${index + 1}`}
-            
+             fill
               className="object-cover"
               sizes="(max-width: 400px) 100vw, 400px"
             />
