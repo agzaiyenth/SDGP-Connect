70d6be19e24e28b07fd83a080e60c8ff11e8e3a8
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-04-10 18:03:39 +0530

    Update DetailsDialog.tsx

--- components/dialogs/DetailsDialog.tsx
+++ components/dialogs/DetailsDialog.tsx
@@ -5,11 +5,11 @@ import { ScrollArea } from '../ui/scroll-area';
 
 const DetailsDialog = ({ open, onOpenChange, projectID }: { open: boolean; onOpenChange: (open: boolean) => void; projectID: any }) => (
   <Dialog open={open} onOpenChange={onOpenChange}>
-    <DialogContent className="max-w-3xl">
+    <DialogContent className="!w-auto !max-w-none md:min-w-[800px] md:max-w-[90vw]">
       <DialogHeader>
         <DialogTitle>Project Details</DialogTitle>
       </DialogHeader>
-      <ScrollArea className="h-[60vh]">
+      <ScrollArea className="h-[70vh]">
         {projectID && <ProjectDetails projectID={projectID} />}
       </ScrollArea>
       <DialogFooter>
