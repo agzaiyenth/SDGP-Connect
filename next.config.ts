cfc5b17e562975d1f47e37592a9af8b6212073dc
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-04-11 19:55:30 +0530

    Add sample-storage.example.com to remote image patterns and create vercel.json for build configuration

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'sample-storage.example.com',
+      },
       
     ],
   },
