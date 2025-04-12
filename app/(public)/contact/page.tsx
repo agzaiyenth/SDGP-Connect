84001c6e83cea332438689abae88fb8e2a2a4852
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-04-12 17:42:28 +0530

    Enhance contact page layout: wrap Contact component in a styled div for improved UI

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,5 +1,8 @@
 import Contact from "@/components/contact/contact-form"
 
 export default function Page() {
-  return <Contact />
-}
+  return (
+  <div className="relative min-h-screen overflow-x-hidden overflow-y-auto py-24 md:py-32 px-4 md:px-32 bg-[#0a0a0a] text-white">
+    <Contact />
+
+  </div>)}
