ae2335d6bdc7eca312a836d274ad0dd1cafceae4
Asra Ameer <agzaiyenth2@gmail.com>
2025-05-05 14:43:13 +0530

    Refactor HeroSection component to simplify Image rendering and improve code readability

--- components/s-project/hero-section.tsx
+++ components/s-project/hero-section.tsx
@@ -9,17 +9,16 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
   coverImage,
 }) => {
   return (
-    <div className="relative w-full h-[70vh] min-h-[500px]">
-      {
-        <Image
-          fill
-          src={coverImage || "https://placehold.co/600x400/png?text=NO+IMAGE"}
-          alt="Project Cover"
-          className="w-full h-full object-cover"
-        />
-      }
+    <div className="relative w-full h-[70vh] min-h-[500px] overflow-hidden">
+      <Image
+        fill
+        src={coverImage || "https://placehold.co/600x400/png?text=NO+IMAGE"}
+        alt="Project Cover"
+        className="object-cover"
+        sizes="100vw"
+        priority
+      />
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-background" />
-      
     </div>
   );
 };
