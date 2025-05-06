7feb4db3c0fc445aeb6e0e504ffa0555a6ca25bb
Asra Ameer <agzaiyenth2@gmail.com>
2025-05-06 23:11:33 +0530

    changed metadata

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SDGP",
-  description: "Where Entrepreneurs are made!",
+  description: "Where Your Future is Shaped",
 };
 
 export default function RootLayout({
