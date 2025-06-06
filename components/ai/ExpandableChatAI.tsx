dfbe40fadd7ad851f6c10be54490e3c7d04f0b99
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-06-06 22:42:45 +0530

    removed upload icon and microphone in the chat box

--- components/ai/ExpandableChatAI.tsx
+++ components/ai/ExpandableChatAI.tsx
@@ -29,9 +29,6 @@ export function ExpandableChatAI() {
     setInput("");
   };
 
-  const handleAttachFile = () => {};
-  const handleMicrophoneClick = () => {};
-
   return (
     <ExpandableChat size="lg" position="bottom-right" icon={<Bot className="h-6 w-6" />}>
       <ExpandableChatHeader className="flex-col text-center justify-center">
@@ -85,14 +82,6 @@ export function ExpandableChatAI() {
             className="min-h-12 resize-none rounded-lg bg-background border-0 p-3 shadow-none focus-visible:ring-0"
           />
           <div className="flex items-center p-3 pt-0 justify-between">
-            <div className="flex">
-              <Button variant="ghost" size="icon" type="button" onClick={handleAttachFile}>
-                <Paperclip className="size-4" />
-              </Button>
-              <Button variant="ghost" size="icon" type="button" onClick={handleMicrophoneClick}>
-                <Mic className="size-4" />
-              </Button>
-            </div>
             <Button type="submit" size="sm" className="ml-auto gap-1.5">
               Send Message
               <CornerDownLeft className="size-3.5" />
