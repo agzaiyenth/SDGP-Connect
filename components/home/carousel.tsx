978e22b73ec370d0158e3e7c61078acda6ec145a
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-06-15 18:01:26 +0530

    Update carousel.tsx

--- components/home/carousel.tsx
+++ components/home/carousel.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
@@ -18,12 +17,12 @@ const images = [
 
 export default function Carousel() {
   const duplicatedImages = [...images, ...images];
-
+  
   return (
     <div className="mb-12 mt-10 relative w-full overflow-hidden h-calc-[calc(100vh-4rem)] md:h-calc-[calc(100vh-6rem)] lg:h-calc-[calc(100vh-8rem)]">
       <div className="absolute top-0 h-full w-32 pointer-events-none z-10 left-0 bg-gradient-to-r from-background to-transparent" />
-      <motion.div 
-        className="carousel-track flex gap-4" 
+      <motion.div
+        className="carousel-track flex gap-4"
         animate={{ x: ['0%', '-50%'] }}
         transition={{ duration: 40, ease: 'linear', repeat: Infinity }}
       >
