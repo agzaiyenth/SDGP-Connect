1a14463912886104c0d1476f6fd890a270a11cbc
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-06-19 09:58:27 +0530

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,10 @@
+// Copyright (c) 2025, Psycode Lab's (https://www.psycodelabs.lk). All Rights Reserved.
+//
+// This software is the property of Psycode Lab's. and its suppliers, if any.
+// Dissemination of any information or reproduction of any material contained
+// herein in any form is strictly forbidden, unless permitted by Psycode Lab's expressly.
+// You may not alter or remove any copyright or other notice from copies of this content.
+
 import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import "../globals.css";
