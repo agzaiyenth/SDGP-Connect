9c663eb8327d7d0b618c059c4ae080e41c4fac84
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-06-29 23:12:36 +0530

    Update NavBarItems.ts

--- data/NavBarItems.ts
+++ data/NavBarItems.ts
@@ -3,8 +3,8 @@ import { Contact, FolderGit2, Home, Info, Upload, Trophy } from "lucide-react";
 export const sidebarItems = [
     { icon: Home, href: '/', label: 'Home' },
     { icon: FolderGit2, href: '/project', label: 'Projects' },
+    { icon: Trophy, href: '/competition', label: 'Competition Winners' },
     { icon: Info, href: '/about', label: 'About Us' },
     { icon: Contact, href: '/contact', label: 'Contact Us' },
     { icon: Upload, href: '/submit', label: 'Submit Details' },
-    { icon: Trophy, href: '/competition', label: 'Competition Winners' },
   ];
