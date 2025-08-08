f1630d005c0a494540fd0e137279c68a9d523dc4
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-08-08 08:29:41 +0530

    Add copyright notice to all main app files
    Inserted a copyright and usage notice for the SDGP Connect project at the top of all major admin, auth, and public-facing page and layout files to clarify ownership and restrict unauthorized use or distribution.

--- app/(public)/blog/[id]/BlogPostContent.tsx
+++ app/(public)/blog/[id]/BlogPostContent.tsx
@@ -1,3 +1,10 @@
+/* Copyright (c) 2025, the contributors of the SDGP Connect project. All Rights Reserved.
+
+This software is the joint property of the contributors to the SDGP Connect project.
+Unauthorized distribution, commercial use, or reproduction of any part of this material
+in any form is strictly prohibited without the explicit written consent of all contributors.
+You may not alter or remove any copyright or attribution notice from this content. */
+
 "use client";
 
 import BlogPostSkeleton from "@/components/blog/BlogPostSkeleton";
