8bb3d37390df9426cf081f2b999861d966d21ab1
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-08-08 08:39:54 +0530

    remove all fake ownership claims

--- data/blogData.ts
+++ data/blogData.ts
@@ -1,9 +1,10 @@
-// Copyright (c) 2025, Psycode Lab's (https://www.psycodelabs.lk). All Rights Reserved.
-//
-// This software is the property of Psycode Lab's. and its suppliers, if any.
-// Dissemination of any information or reproduction of any material contained
-// herein in any form is strictly forbidden, unless permitted by Psycode Lab's expressly.
-// You may not alter or remove any copyright or other notice from copies of this content.
+/* Copyright (c) 2025, the contributors of the SDGP Connect project. All Rights Reserved.
+
+This software is the joint property of the contributors to the SDGP Connect project.
+Unauthorized distribution, commercial use, or reproduction of any part of this material
+in any form is strictly prohibited without the explicit written consent of all contributors.
+You may not alter or remove any copyright or attribution notice from this content. */
+
 export interface BlogPost {
   id: string;
   title: string;
